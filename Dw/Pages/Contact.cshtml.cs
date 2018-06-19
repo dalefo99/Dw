a5a4a9e02f296b4963c916d9f8c5085ca79f8975
Dale Foshe <Dale.Foshe@AcumenMD.com>
2018-06-19 15:58:25 -0400

    Added contact.

--- Dw/Pages/Contact.cshtml.cs
+++ Dw/Pages/Contact.cshtml.cs
@@ -1,18 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Dogwood.Pages
 {
     public class ContactModel : PageModel
     {
-        public string Message { get; set; }
+        [BindProperty]
+        public ContactFormModel Contact { get; set; }
+
 
         public void OnGet()
         {
-            Message = "Your contact page.";
+            
         }
+
+
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var mailbody = $@"Hallo website owner,
+
+            This is a new contact request from your website:
+
+            Name: {Contact.Name}
+            Email: {Contact.Email}
+            Message: {Contact.Message}";
+
+            ViewData["Test"] = mailbody;
+
+            return RedirectToPage("Contact");
+        }
+    }
+
+    public class ContactFormModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
     }
 }
