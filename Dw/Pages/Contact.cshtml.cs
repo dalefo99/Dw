f3def60843b49e6079e821e93b48af1d0d02327a
Dale Foshe <dale@foshe.org>
2018-06-24 22:48:02 -0400

    Added challenge pages

--- Dw/Pages/Contact.cshtml.cs
+++ Dw/Pages/Contact.cshtml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace Dogwood.Pages
 {
@@ -16,7 +19,7 @@ namespace Dogwood.Pages
 
         public void OnGet()
         {
-            
+
         }
 
 
@@ -25,22 +28,49 @@ namespace Dogwood.Pages
         {
             if (!ModelState.IsValid)
             {
+                ViewData["Test"] = "test";
                 return Page();
             }
+            
 
             var mailbody = $@"Hallo website owner,
-
+            
             This is a new contact request from your website:
-
+            
             Name: {Contact.Name}
             Email: {Contact.Email}
             Message: {Contact.Message}";
 
-            ViewData["Test"] = mailbody;
+            var smtpClient = new SmtpClient
+            {
+                Host = "smtp.gmail.com", // set your SMTP server name here
+                Port = 587, // Port 
+                EnableSsl = true,
+                Credentials = new NetworkCredential("dale@foshe.org", "spuDC@mera99")
+            };
 
-            return RedirectToPage("Contact");
+
+
+            using (var message = new MailMessage(Contact.Email, "dale@foshe.org"))
+            {
+                message.To.Add(new MailAddress("dale@foshe.org"));
+                message.From = new MailAddress(Contact.Email);
+                message.Subject = "New E-Mail from my website";
+                message.Body = mailbody;
+
+                //await smtpClient.SendMailAsync(message);
+            }
+
+
+            System.Threading.Thread.Sleep(5000);
+
+            //return RedirectToPage("Contact");
+            ViewData["Sent"] = "Message Successfully Sent!";
+            return Page();
         }
     }
+
+
 
     public class ContactFormModel
     {
